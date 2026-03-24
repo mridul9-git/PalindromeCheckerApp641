fff3c4fdbe27896ff857a1b18fc79d5e256c18e4
mridul9-git <mridulthakur96@gmail.com>
2026-03-24 10:58:17 +0530

    UC2

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
 import java.util.*;
 
 public class PalindromeApp{
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         System.out.println("=== Palindrome Checker Management System ===");
         System.out.println("-------------RA642-------------");
 
 //UC1
         System.out.println("\nUC1 - Basic Reverse Check");
-        String word1 = "MalayalaM";
-        String rev1 = "";
-        for (int i = word1.length() - 1; i >= 0; i--) {
-            rev1 += word1.charAt(i);
-        }
-        System.out.println(word1 + " -> " + (word1.equals(rev1) ? "Palindrome" : "Not Palindrome"));
+        String word1="MalayalaM";
+        String rev1="";
+        for(int i=word1.length()-1;i>=0;i--){rev1+=word1.charAt(i);}
+        System.out.println(word1+" -> "+(word1.equals(rev1)?"Palindrome":"Not Palindrome"));
+
+//UC2
+        System.out.println("\nUC2 - StringBuilder Reverse");
+        String word2="maLAYALam";
+        System.out.println(word2+" -> "+(word2.equals(new StringBuilder(word2).reverse().toString())?"Palindrome":"Not Palindrome"));
+
     }}
